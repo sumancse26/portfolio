b83d1bda0294181d99a89ad123bd812bdbe872c0
DESKTOP-C94S9FL\suman <suman.iu32@gmail.com>
2022-07-03 22:39:30 +0600

    Dev
    navbar js file created

---
+++ navbar.js
@@ -0,0 +1,30 @@
+export default navmenu = [
+    {
+        "id": 1,
+        "name": "Home"
+    },
+    {
+        "id": 2,
+        "name": "About"
+    },
+    {
+        "id": 3,
+        "name": "Skills"
+    },
+    {
+        "id": 4,
+        "name": "Services"
+    },
+    {
+        "id": 5,
+        "name": "Projects"
+    },
+    {
+        "id": 6,
+        "name": "Blog"
+    },
+    {
+        "id": 7,
+        "name": "Contact"
+    },
+]
